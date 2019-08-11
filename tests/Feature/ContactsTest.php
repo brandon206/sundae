074278055f45e9aad8601e8df07d631a59efafc3
Brandon Park <brandonpark206@gmail.com>
2019-08-10 22:20:53 -0700

    adding basic validation to unit tests

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -14,8 +14,47 @@ class ContactsTest extends TestCase
     public function a_contact_can_be_added ()
     {
         $this->withoutExceptionHandling();
-        $this->post('/api/contacts', ['name' => 'Test Name']);
+        $this->post('/api/contacts', [
+            'name' => 'Test Name',
+            'email' => 'test@email.com',
+            'birthday' => '05/14/1999',
+            'company' => 'ABC Company',
+        ]);
+
+        $contact = Contact::first();
  
-        $this->assertCount(1, Contact::all());
+        $this->assertEquals('Test Name', $contact->name);
+        $this->assertEquals('test@email.com', $contact->email);
+        $this->assertEquals('05/14/1999', $contact->birthday);
+        $this->assertEquals('ABC Company', $contact->company);
+
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+
+        $response = $this->post('/api/contacts', [
+            'email' => 'test@email.com',
+            'birthday' => '05/14/1999',
+            'company' => 'ABC Company',
+        ]);
+ 
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /** @test */
+    public function email_is_required()
+    {
+
+        $response = $this->post('/api/contacts', [
+            'name' => 'Test',
+            'birthday' => '05/14/1999',
+            'company' => 'ABC Company',
+        ]);
+ 
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
     }
 }
