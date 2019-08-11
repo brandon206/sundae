ba271c8aa549a919ad7d6237564c0ec444b60a24
Brandon Park <brandonpark206@gmail.com>
2019-08-10 21:58:00 -0700

    init commit

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    public function store()
+    {
+        Contact::create([
+            'name' => request('name'),
+        ]);
+    }
+}
---
+++ tests/Feature/ContactsTest.php
@@ -0,0 +1,21 @@
+<?php
+ 
+namespace Tests\Feature;
+ 
+use App\Contact;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+ 
+class ContactsTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function a_contact_can_be_added ()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/api/contacts', ['name' => 'Test Name']);
+ 
+        $this->assertCount(1, Contact::all());
+    }
+}
