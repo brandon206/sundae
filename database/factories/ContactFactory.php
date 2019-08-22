8c6f24297cad5c5d7909e44cb1aa6c49c7214c97
Brandon Park <brandonpark206@gmail.com>
2019-08-21 20:57:34 -0700

    retreiving a specific contact

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Contact;
+use Faker\Generator as Faker;
+
+$factory->define(Contact::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'birthday' => '05/14/1999',
+        'company' => $faker->company,
+    ];
+});
