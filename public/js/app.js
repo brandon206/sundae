82f92511ed648b146bb862f8d55b6505abc31e41
brandon <brandonpark206@gmail.com>
2020-01-11 12:39:17 -0800

    changing virtual machine from xampp to mamp

--- public/js/app.js
+++ public/js/app.js
@@ -34810,8 +34810,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vue_router__WEBPACK_IMPORTED_MODU
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/sundae/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/sundae/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/brandonpark/Desktop/lfz/sundae/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/brandonpark/Desktop/lfz/sundae/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
